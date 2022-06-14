f6f67faf86c01eb57bd4e9a8acae4af7eceb1acc
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-06-15 00:07:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Cards from "./components/Cards";
+import Header from "./components/Header";
+
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<Cards />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/configs.js
@@ -0,0 +1,4 @@
+var configs = {
+	token: "Mxf2qdU7bq7BHWkNWWrHImiSbIDzgrXd4YCk4bXy6lc"
+};
+module.exports = configs;
